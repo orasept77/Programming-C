fc38ad9c10fe07b32d03ba885da2daabb224841a
Georeperboleja <ishuckn5@gmail.com>
2023-08-11 22:14:55 +0200

    Update Lekcja.c

--- Example/Lekcja.c
+++ Example/Lekcja.c
@@ -3,53 +3,55 @@
 #define COL 2
 #define ROW 3
 
-typedef struct abc
-{
+typedef struct abc {
     int a;
-}abc ;
+} abc;
 
-
-void matrix_multiplication(float* tab,int col, int row, float x){
-     for (int i = 0; i < col*row; i++)
-    {
-        tab[i]*=x;
+void matrix_multiplication(float *tab, int col, int row, float x) {
+    for (int i = 0; i < col * row; i++) {
+        tab[i] *= x;
     }
 }
 
-void print_matrix(float* tab, int col, int row){
-    for (int i = 0; i < col*row; i++)
-    {
+void print_matrix(float *tab, int col, int row) {
+    for (int i = 0; i < col * row; i++) {
         printf("%.2f\t", tab[i]);
-        if((col-1) == (i%col) ){
+        if ((col - 1) == (i % col)) {
             printf("\n");
         }
     }
 }
 
-int main(int argc, char const *argv[])
-{
-    float tab[3][2] = { {1.0,2.0},{2.0,3.0}, {3.0,3.0} };
+int main(int argc, char const *argv[]) {
+    float tab[ROW][COL] = {
+        {1.0, 2.0},
+        {2.0, 3.0},
+        {3.0, 3.0}
+    };
 
-    print_matrix(tab, 2,3);
+    printf("Original Matrix:\n");
+    print_matrix(tab, COL, ROW);
 
-    matrix_multiplication(tab,2,3,4);
+    matrix_multiplication(&tab[0][0], COL, ROW, 4);
 
-    print_matrix(tab, 2,3);
+    printf("\nMatrix After Multiplication:\n");
+    print_matrix(tab, COL, ROW);
 
-     // save to file
-    FILE *f = fopen("txt1.txt", "w");
-    if (f == NULL)
-    {
+    // Save to file
+    FILE *file = fopen("txt1.txt", "w");
+    if (file == NULL) {
         printf("Error opening file!\n");
         return -1;
     }
-    for (int i = 0; i < ROW; i++)
-    {
-        for (int j = 0; j < COL; j++)
-            fprintf(f, "%.4f ", tab[i][j]);
-        fprintf(f, "\n");
+
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            fprintf(file, "%.4f ", tab[i][j]);
+        }
+        fprintf(file, "\n");
     }
-    fclose(f);
+
+    fclose(file);
 
     return 0;
 }
